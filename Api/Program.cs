77d7fa21ba8de4201e909d4c1de848d62f4de728
Balazs Kereszturi <19balazs86@gmail.com>
2023-03-16 10:49:34 +0100

    Refactoring: basic things

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,19 +1,15 @@
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Azure.Functions.Worker.Configuration;
 
-namespace ApiIsolated
+namespace BlazorApp.Api;
+
+public static class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main()
-        {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
-                .Build();
+        IHost host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .Build();
 
-            host.Run();
-        }
+        host.Run();
     }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,33 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorApp.Client;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace BlazorApp.Client;
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
 
-await builder.Build().RunAsync();
+        WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        IServiceCollection services  = builder.Services;
+        IConfiguration configuration = builder.Configuration;
+
+        var baseAddress = new Uri(configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress);
+
+        // Add services to the container
+        {
+            services.addDefaultHttpClient(baseAddress);
+        }
+
+        await builder.Build().RunAsync();
+    }
+
+    private static void addDefaultHttpClient(this IServiceCollection services, Uri baseAddress)
+    {
+        services.AddSingleton(new HttpClient { BaseAddress = baseAddress });
+    }
+}
